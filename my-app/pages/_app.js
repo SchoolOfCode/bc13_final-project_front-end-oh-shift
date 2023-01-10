4dd38baa89df61dff1e7f695d3bd819c699a452c
Ikram-Zak <112779062+Ikram-Zak@users.noreply.github.com>
2023-01-10 10:01:51 +0000

    initialisation of the repo

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <h1>hellooooooooooo world</h1>
+
+    </div>
+  )
 }
+
+//<Component {...pageProps} />
