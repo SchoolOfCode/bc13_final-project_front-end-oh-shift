c5019b616238cf49647db8cdeed901dbb90a024b
soph-th <109481663+soph-th@users.noreply.github.com>
2023-01-15 22:21:09 +0000

    test card added, filter started, individual games pages and routing added, need to pass onchange filter values to gamelist component

--- my-app/components/Display/display.js
+++ my-app/components/Display/display.js
@@ -1,15 +1,38 @@
 import React from 'react'
 import FilterBar from '../FilterBar/fiterBar'
-
+import { useState, useEffect } from 'react';
 import GamecardList from '../GameCardLIst/gamCardList'
 
 function Display() {
+
+  const [games, setGames] = useState();
+  const [genreFilter, setGenreFilter] = useState('');
+  const [numberOfPlayersFilter, setNumberOfPlayersFilter] = useState('');
+
+  const fetchUrl = 'https://oh-shift.onrender.com/api/games/'
+
+  async function getData() {
+    const res = await fetch(`${fetchUrl}?genre=${genreFilter}&number_of_players=${numberOfPlayersFilter}`);
+    const data = await res.json();
+    setGames(data.payload);
+    console.log('getData called')
+    console.log(games)
+    console.log(`${fetchUrl}?genre=${genreFilter}&number_of_players=${numberOfPlayersFilter}`)
+  }
+  
+  useEffect(() => {
+    getData();
+  }, []);
+
+
   return (
+    (games) ? 
   <>
     <div><FilterBar/></div>
-    <GamecardList></GamecardList>
+    <GamecardList games={games}></GamecardList>
     
 </>
+: <p>Sorry, no results found. Lower your standards and reduce your filter options!</p>
   )
 }
 
