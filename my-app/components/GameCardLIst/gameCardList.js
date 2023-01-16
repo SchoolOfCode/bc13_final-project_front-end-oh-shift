e0135082db95ec1bc2eabd816b3657117481551e
Federica Costantino <ricacostt@gmail.com>
2023-01-16 14:20:22 +0000

    fix duplicate components and fetch request

--- my-app/components/GameCardLIst/gameCardList.js
+++ my-app/components/GameCardLIst/gameCardList.js
@@ -1,19 +1,17 @@
 import React from "react";
 import Gamecard from "../Gamecard/gamecard";
-import { useState, useEffect } from "react";
-import Card from "../Card/Card.js";
 
-function GamecardList({games}) {
+function GameCardList({games}) {
 console.log('gamCardList', games)
 
   return (
     <div>
       {games.map((game) => (
-        <Card game={game} key={game.id}/>
+        <Gamecard game={game} key={game.id}/>
       ))}
 
     </div>
   );
 }
 
-export default GamecardList;
+export default GameCardList;
