27676b9128f921b400cdc2d108fc686cabe3d173
ricacostt <ricacostt@gmail.com>
2023-01-18 11:07:46 +0000

    card component added, add function to display just the first 20 words of the description

--- my-app/components/Gamecard/gamecard.js
+++ my-app/components/Gamecard/gamecard.js
@@ -2,11 +2,46 @@ import styles from "./gamecard.module.css";
 import Badge
  from "../Badge/Badge";
 function Gamecard({game}) {
+let description = game.description
 
-  console.log(game)
+
+let wordCount = 20;
+let words = description.split(" ");
+let shortDescription = words.slice(0, wordCount).join(" ") + "...";
+console.log(shortDescription);
   return (
       //SOPHIE CARD
-      <div >
+<div className="card w-96 bg-base-100 shadow-xl" style={{marginBottom:"2rem"}}>
+  <figure><img src={game.artwork_image_url} alt="game image" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">
+     {game.title}
+    </h2>
+    
+    <p>{shortDescription}</p>
+    <div className="card-actions justify-end">
+        <Badge key={game.minimum_players} label={`${game.minimum_players}-${game.maximum_players} players`}/>
+        <Badge key={game.difficulty} label={game.difficulty}/>
+        <Badge key={game.minimum_age} label={`${game.minimum_age}+`}/>
+        <Badge key={game.duration} label={`${game.duration} mins`}/>
+        {game.genre?.map((gameGenre) => {
+        console.log(game.genre)
+        return (<Badge key={gameGenre} label={gameGenre}/>)}
+        )}
+        </div>
+      <div className="badge badge-warning">NEW</div>
+    <div className="card-actions justify-end">
+      <div className="badge badge-outline">Fashion</div> 
+      <div className="badge badge-outline">Products</div>
+    </div>
+  </div>
+</div>
+ );
+}
+
+
+
+      {/* <div >
 <div className="cardcontainer" style={{marginBottom:"2rem"}}>
   <div className="card w-96 shadow-xl image-full">
   <figure>
@@ -26,13 +61,11 @@ function Gamecard({game}) {
     <p>{game.description}</p>
     <div className="card-actions justify-end">
       <button className="btn btn-primary"><a href={`/games/${game.id}`}>Read More</a></button>
-    </div>
+    </div> */}
    
-  </div>
-</div>
-</div>
-</div>
-  );
-}
-
+//   </div>
+// </div>
+// </div>
+// </div>
+ 
 export default Gamecard;
