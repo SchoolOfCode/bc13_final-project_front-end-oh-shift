d0259d1afddc1c5e429d39d5057e21b6f9b50d90
[Creg Grady] <112422397+CregCodes@users.noreply.github.com>
2023-01-21 14:10:44 +0000

    set up branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-import React, {useState, useContext} from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import React, { useState } from "react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
-export const DarkModeWrapper = React.createContext()
+export const DarkModeWrapper = React.createContext();
 
 export default function App({ Component, pageProps }) {
-  const [darkMode, setDarkMode] = useState(false)
+  const [darkMode, setDarkMode] = useState(false);
   return (
-    <DarkModeWrapper.Provider value={{darkMode,setDarkMode}}>
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
-  </DarkModeWrapper.Provider>
-  )
+    <DarkModeWrapper.Provider value={{ darkMode, setDarkMode }}>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </DarkModeWrapper.Provider>
+  );
 }
-
