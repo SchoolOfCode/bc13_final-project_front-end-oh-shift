23e31390a4bb0f5c910511acc6787c1718851b61
Soph T <109481663+soph-th@users.noreply.github.com>
2023-01-26 16:46:00 +0000

    Update AddReview.test.js

--- components/AddReview/AddReview.test.js
+++ components/AddReview/AddReview.test.js
@@ -3,7 +3,7 @@ import { render, screen, fireEvent } from "@testing-library/react";
 //test ('Add a review')
 
 
-/*
+
 test('render rating', () => {
     render(<StyledRating />);
     const idElement = screen.getByText("5 Stars");
